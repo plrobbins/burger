4bddfb7dcb71b9fdfd47fef1536423c77479da38
Philip Robbins <plrobbins68@outlook.com>
2020-10-14 23:37:20 -0400

    adding code to the repo. First save

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Bacon Double Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
+INSERT INTO burgers (name) VALUES ('Blue Cheese bacon Burger');
